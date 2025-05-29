1dd4ac6237bc6fcf28f97d50cce5e49a232ee4b0
b1rin <141683940+b1rin@users.noreply.github.com>
2025-05-29 18:39:38 +0300

    Add files via upload

--- client.c
+++ client.c
@@ -34,7 +34,7 @@ int main(int argc, char* argv[]) {
     }    
 
     // sending information
-    char message[256] = "Sukhorukov Alexander Vladislavovich KKSO-26-24 1 kurs";
+    char message[256] = "Kikolenko Vladislav  KKSO-26-24 1 kurs";
     send(network_socket, message, strlen(message), 0);
 
 	// then close the socket
